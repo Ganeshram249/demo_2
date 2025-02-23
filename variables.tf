30262951f19ccb31daff1f1b4172f7b32fd5a3f0
Ganeshram249 <157260675+Ganeshram249@users.noreply.github.com>
2025-02-23 16:36:22 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -27,3 +27,39 @@ variable "subnet_tags" {
   description = "Tags for the subnet"
   type        = map(string)
 }
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "The AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The instance type for the EC2 instance"
+  type        = string
+}
+
+variable "key_name" {
+  description = "The key pair name for the EC2 instance"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "The subnet ID for the EC2 instance"
+  type        = string
+}
+
+variable "security_group_ids" {
+  description = "The security group IDs for the EC2 instance"
+  type        = list(string)
+}
+
+variable "ec2_tags" {
+  description = "Tags for the EC2 instance"
+  type        = map(string)
+}
+
