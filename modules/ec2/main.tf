2f5ec8b475e2afc768cedc7371b77caddf0a2d0c
Ganeshram249 <157260675+Ganeshram249@users.noreply.github.com>
2025-02-23 16:19:42 +0530

    Create main.tf

---
+++ modules/ec2/main.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "ec2_instance" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  subnet_id     = var.subnet_id
+  vpc_security_group_ids = var.security_group_ids
+
+  tags = var.tags
+}
+
+output "instance_id" {
+  value = aws_instance.ec2_instance.id
+}
